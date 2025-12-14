70523776d119ff5ca16c66a017a3a45028f69073
Rajaryan singh <raj.aryan2518@gmail.com>
2025-12-14 21:05:00 +0530

    message Schema added

--- backend/models/Thread.js
+++ backend/models/Thread.js
@@ -16,3 +16,31 @@ const messageSchema = new mongoose.Schema({
         default: Date.now
     }
 })
+
+
+const ThreadSchema = new mongoose.Schema({
+    threadId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    title: {
+        type: String,
+        default: 'New Chat'
+    },
+    messages: [messageSchema],
+    createdAt:  {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+export default mongoose.model("Thread", ThreadSchema);
+
+
