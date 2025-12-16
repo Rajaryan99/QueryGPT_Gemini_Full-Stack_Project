157593617a712bfe3e32f1a974f49b8e4b68ad93
Rajaryan singh <raj.aryan2518@gmail.com>
2025-12-16 13:23:25 +0530

    sidebar nav button

---
+++ frontend/src/components/ChatWindow.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ChatWindow.css'
+
+export default function ChatWindow() {
+  return (
+    <div>
+      <h1>ChatWindow</h1>
+    </div>
+  )
+}
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Sidebar.css'
+
+export default function Sidebar() {
+  return (
+    <section className='sidebar'>
+      {/* new chat button */}
+
+      <button>
+        <img src="src/assets/blacklogo.png" alt="logo" className='logo' />
+
+        <span><i class="fa-regular fa-pen-to-square"></i></span>
+      </button>
+
+      {/* history */}
+
+        <ul className='history'>
+          <li>history1</li>
+          <li>history1</li>
+          <li>history1</li>
+        </ul>
+      {/* sign */}
+
+      <div className='sign'>
+        <p>QueryGPT &hearts;</p>
+      </div>
+      
+    </section>
+  )
+}
