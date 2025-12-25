5e07ca90c57b0d0effe01ff1c47323d9b7117e23
Rajaryan singh <raj.aryan2518@gmail.com>
2025-12-25 12:12:47 +0530

    backend url

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: `${process.env.FRONDEND_URL}`,
   credentials: true
 }))
 
